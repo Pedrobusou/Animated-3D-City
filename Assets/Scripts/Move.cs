34e96ac8c6d795b0427d7e1bcaadaea6fcbbc3ad
Pedro <pedrobusou@gmail.com>
2019-09-15 21:18:56 +0200

    Refactor Move script

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,17 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Move: MonoBehaviour
+public class Move : MonoBehaviour
 {
     public int speed = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
