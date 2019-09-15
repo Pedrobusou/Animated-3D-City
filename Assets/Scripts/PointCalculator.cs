d1fe9ad95280db2a1b8a3fc5b8cc4dfcec0d4dfc
Pedro Ramos González <pedrobusou@gmail.com>
2019-09-15 16:00:59 +0200

    Initial commit

---
+++ Assets/Scripts/PointCalculator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PointCalculator
+{
+    public int CalculateTotalPoints(int killedEnemies, int killedVillagers, int multiplicator)
+    {
+        int points = 0;
+
+        points += killedEnemies * 100;
+        points += Mathf.FloorToInt(killedEnemies / 10) * 500;
+        points -= killedVillagers * 100;
+
+        if (killedVillagers > 10)
+        {
+            multiplicator = 1;
+        }
+
+        points *= multiplicator;
+
+        return points;
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+[![BCH compliance](https://bettercodehub.com/edge/badge/Pedrobusou/unity-base-project?branch=master)](https://bettercodehub.com/)
+[![Unity Version](https://img.shields.io/badge/unity%20version%20-2019.2.2f1-blue.svg)](https://unity3d.com/unity/whats-new/2019.2.2)
+
+# Unity base project
+
+References:
+https://thoughtbot.com/blog/how-to-git-with-unity
+https://docs.unity3d.com/Manual/SmartMerge.html
