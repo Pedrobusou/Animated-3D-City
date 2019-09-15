ddcfc19e16e7f3751606e26ec863db352a66a968
Pedro <pedrobusou@gmail.com>
2019-09-15 20:58:26 +0200

    Add move script
    added to all vehicles

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour
+{
+    public int speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
